b5afc3d80a1d9273678c1daf8b200fdcc88e0602
yyc12345 <yyc12321@outlook.com>
2023-06-27 21:30:44 +0800

    update files

---
+++ Scripts/generate_source.sh
@@ -0,0 +1,4 @@
+./NlpEncoder/out/NlpEncoder uncompress NlpSrc/VT25.nlp NlpSrc/VT25.txt
+./NlpEncoder/out/NlpEncoder uncompress NlpSrc/VT35.nlp NlpSrc/VT35.txt
+./NlpEncoder/out/NlpEncoder uncompress NlpSrc/VT40.nlp NlpSrc/VT40.txt
+./NlpEncoder/out/NlpEncoder uncompress NlpSrc/VT50.nlp NlpSrc/VT50.txt
